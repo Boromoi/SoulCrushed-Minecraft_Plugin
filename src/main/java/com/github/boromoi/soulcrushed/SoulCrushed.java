2a04e7adafd24c9d1ccd54b8d0ab93ddd7cede37
LiamVanSlingerlandt <Liam.van.slingerlandt@hva.nl>
2023-08-27 23:56:36 +0200

    - Made it so there is a welcome message when a player joins. - Made it so the players last location get's broadcasted to all players when a player dies.

--- src/main/java/com/github/boromoi/soulcrushed/SoulCrushed.java
+++ src/main/java/com/github/boromoi/soulcrushed/SoulCrushed.java
@@ -2,10 +2,15 @@ package com.github.boromoi.soulcrushed;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
 public final class SoulCrushed extends JavaPlugin  implements Listener  {
 
         @Override
@@ -16,7 +21,26 @@ public final class SoulCrushed extends JavaPlugin  implements Listener  {
 
         @EventHandler
         public void onPlayerJoin(PlayerJoinEvent event) {
-            event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+            // Send message to specific player who joins the server.
+            //event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+            // Broadcast message about the player who joins the server.
+            Bukkit.broadcast(Component.text("Hello, " + event.getPlayer().getName() + " kindled one thou better not die soon..."));
+        }
+
+        @EventHandler
+        public void onPlayerDying(PlayerDeathEvent event) {
+            // Send the player's death location in chat to the player who died.
+            //Bukkit.getConsoleSender().sendMessage(event.getPlayer().getLastDeathLocation().toString());
+
+            Player player = event.getPlayer();
+
+            // Send the player's death location in chat to every player online.
+            Bukkit.broadcast(Component.text("X: " + player.getLastDeathLocation().getBlockX() + " Y: " + player.getLastDeathLocation().getBlockY() + " Z: " + player.getLastDeathLocation().getBlockZ()));
+        }
+
+        @EventHandler
+        public void onPlayerRespawn(PlayerRespawnEvent event) {
+        // Set the max hp of the player depending on a death integer.
         }
 }
 
